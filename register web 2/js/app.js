a99815588ad6263473f32797d86dbea84356c021
Qurbonoff <159319793+Qurbonoff11@users.noreply.github.com>
2025-05-03 15:43:03 +0500

    add

--- register web 2/js/app.js
+++ register web 2/js/app.js
@@ -1,40 +1,33 @@
 const users = [
   {
-    username: "massa.91",
-    password: "uzbekistan",
-    firstName: "Shuxratbek",
-    image: "../assets/img/massa.jpg"
+    username: "minor",
+    password: "LightDream",
   },
   {
     username: "asl_wayne",
-    password: "uzbekistan212",
-    firstName: "Asilbek",
-    image: "../assets/img/asl_wayne.jpg"
-  },
-  {
-    username: "minor",
-    password: "LightDream",
-    firstName: "Rustam",
-    image: "../assets/img/m1nor.jpg"
+    password: "uzbekistan212"
   },
   {
     username: "Uzboom",
-    password: "Uzboom.11",
-    firstName: "Shovkat",
-    image: "../assets/img/uzboom.jpg"
+    password: "Uzboom.11"
   },
   {
-    username: "Uzmir",
-    password: "LightdreamUzmir",
-    firstName: "Bahodir",
-    image: "../assets/img/uzm.jpg"
+    username: "massa.38",
+    password: "uzbekistan"
   },
 ];
 
 const usernameInput = document.querySelector("#userName");
 const passwordInput = document.querySelector("#userPass");
 const loginBtn = document.querySelector("#login");
-const textContent = document.querySelector("#content")
+const textContent = document.querySelector("#content");
+
+const user1 = document.querySelector(".user-1")
+const user2 = document.querySelector(".user-2")
+const user3 = document.querySelector(".user-3")
+const user4 = document.querySelector(".user-4")
+
+// console.log(userImg.src);
 
 loginBtn.addEventListener("click", () => {
   const foundUser = users.find(
@@ -43,14 +36,26 @@ loginBtn.addEventListener("click", () => {
       user.password === passwordInput.value
   );
 
+
   if (foundUser) {
-    textContent.textContent = "Success"
-    textContent.style = "color: green"
+    textContent.textContent = "Success";
+    textContent.style = "color: green";
     console.log("Success");
+
     window.location.href = "../src/userPage.html"
+
+    if (foundUser.username === "minor") {
+      user1.style.display = "flex"
+    } else if (foundUser.username === "asl_wayne"){
+      user2.style.display = "flex"
+    } else if (foundUser.username === "Uzboom"){
+      user2.style.display = "flex"
+    } else if (foundUser.username === "massa.38") {
+      user2.style.display = "flex"
+    }
   } else {
-    textContent.textContent = "Invalid username or password"
-    textContent.style = "color: red"
+    textContent.textContent = "Invalid username or password";
+    textContent.style = "color: red";
     console.log("Error");
   }
 });
